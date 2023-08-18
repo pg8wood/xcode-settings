db19bb55a0badc2d309de851d58a54c7d20120a0
Patrick Gatewood <patrick@patrickgatewood.com>
2023-08-18 14:38:15 -0400

    Commit recent snippets & update setup script

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ set -v
 
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets/* .
 rmdir ~/Library/Developer/Xcode/UserData/CodeSnippets/
-ln -s ~/Documents/xcode-snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+ln -s $PWD ~/Library/Developer/Xcode/UserData/CodeSnippets
